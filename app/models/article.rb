92026d5096a5b8fbada97afae83d148307b6fed7
Mahmoud Yehia <eng.mdodo94@gmail.com>
2019-01-03 18:36:59 +0000

    create Article model and set validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
+    validates :title ,presence: true, length: {minimum: 3, maximum: 50}
+    validates :descripton ,presence: true ,length: {minimum: 10, maximum: 500}
     
 end
