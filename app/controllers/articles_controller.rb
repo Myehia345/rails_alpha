7636147f8ba772092fe785c9cde117b8c1219cc8
Mahmoud Yehia <eng.mdodo94@gmail.com>
2019-01-11 01:21:55 +0000

    Create, show ,update from UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,39 @@ class ArticlesController < ApplicationController
    def new
        @article = Article.new
    end
+
+   def edit
+       
+       @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(article_param)
-        @article.save
-        redirect_to_article_path(@article)
+        
+        if @article.save
+            flash[:notice] = "Article Sucessfully Created!"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+        
     end
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_param)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+        
+    end
+    
+    def show
+       @article = Article.find(params[:id])
+    end
+    
     private
     def article_param
        params.require(:article).permit(:title,:descripton) 
