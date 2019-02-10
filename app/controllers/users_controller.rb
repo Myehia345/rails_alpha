e75498e3b7eeba4dc454aa57776e4ed67f571dc3
Mahmoud Yehia <eng.mdodo94@gmail.com>
2019-02-10 00:20:50 +0000

    list all users and link user to articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,9 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
    end
    
+   def index
+      @user = User.all
+   end
 #-----------------------------------------------------------------------------------   
    private
    def user_params
